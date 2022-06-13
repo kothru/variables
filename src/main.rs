7a7bb5d94c1b61dad2266f6e197104772654e44f
kothru <1894683+kothru@users.noreply.github.com>
2022-06-13 21:52:27 +0900

    method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use std::io;
+
 fn main() {
-    let x = 5;
-    println!("value of x {}", x);
-    let x = 6;
-    println!("value of x {}", x);
+    let a = [1, 2, 3, 4, 5];
+    println!("input");
+    let mut index = String::new();
+    io::stdin().read_line(&mut index).expect("failed");
 
-    // const THREE_HOURS: u32 = 60 * 60;
+    let index: usize = index.trim().parse().expect("wrong num");
+    let element = a[index];
+    println!("value index {} is {}", index, element);
 
-    // let guess = "42".parse().expect("not number");
+    let y = {
+        let x = 3;
+        x + 1
+    };
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
