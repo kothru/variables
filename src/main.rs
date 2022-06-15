72833ada4c9914053d53fb1ba340cb894f410391
kothru <1894683+kothru@users.noreply.github.com>
2022-06-15 22:29:54 +0900

    copy

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,14 @@ fn main() {
         let x = 3;
         x + 1
     };
+
+    // Heap Datatype need clone to copy, if x steady use
+    // let x = String::from("test");
+    // let y = x.clone();
+    // Stack Datatype only deep copy
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
 }
 
 fn plus_one(x: i32) -> i32 {
