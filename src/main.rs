87665d3448f10a0aab3690d097e1bc154f816801
kothru <1894683+kothru@users.noreply.github.com>
2022-06-19 17:13:44 +0900

    use

--- src/main.rs
+++ src/main.rs
@@ -151,3 +151,18 @@ enum IpAddr {
     V4(String),
     V6(String),
 }
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+// use crate::front_of_house::hosting;
+mod customer {
+    use crate::front_of_house::hosting;
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+        // super::front_of_house::hosting::add_to_waitlist();
+    }
+}
