76de8413be0a60e00c577abf8aa228c168ccc1df
kothru <1894683+kothru@users.noreply.github.com>
2022-06-20 22:18:02 +0900

    vec

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,12 @@ mod front_of_house;
 pub use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
+    // init vector
+    let v: Vec<i32> = Vec::new();
+    // vec init macro
+    let v = vec![1, 2, 3];
+    match v.get(2) {
+        Some(third) => println!("some {}", third),
+        None => println!("none"),
+    }
 }
