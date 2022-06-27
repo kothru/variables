6ddc513168f09e9a10b974ccb636c3b109d16bd7
kothru <1894683+kothru@users.noreply.github.com>
2022-06-28 07:16:14 +0900

    generics

--- src/lib.rs
+++ src/lib.rs
@@ -88,7 +88,29 @@ pub fn eat_at_restaurant() {
     // panic! use invalid value
     // like impl type domain get invalid value
     // almost use Result
+
+    // let number_list = vec![34, 50, 25, 100, 65];
+
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
+
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
 }
+
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
 
 fn read_username_from_file() -> Result<String, io::Error> {
     // ? operator is ReturnedError if error
