23eeb36da61950e25124e1549368bc9b9663b980
kothru <1894683+kothru@users.noreply.github.com>
2022-06-29 08:14:16 +0900

    trait return

--- src/lib.rs
+++ src/lib.rs
@@ -157,6 +157,7 @@ impl Summary for Tweet {
 }
 
 // function can return which has a trait
+// can return single type
 fn returns_summarizable() -> impl Summary {
     Tweet {
         username: String::from("horse_ebooks"),
@@ -165,3 +166,25 @@ fn returns_summarizable() -> impl Summary {
         retweet: false,
     }
 }
+
+// cannot return multiple type
+// fn returns_summarizable(switch: bool) -> impl Summary {
+//     if switch {
+//         NewsArticle {
+//             headline: String::from("Penguins win the Stanley Cup Championship!"),
+//             location: String::from("Pittsburgh, PA, USA"),
+//             author: String::from("Iceburgh"),
+//             content: String::from(
+//                 "The Pittsburgh Penguins once again are the best \
+//                  hockey team in the NHL.",
+//             ),
+//         }
+//     } else {
+//         Tweet {
+//             username: String::from("horse_ebooks"),
+//             content: String::from("of course, as you probably already know, people"),
+//             reply: false,
+//             retweet: false,
+//         }
+//     }
+// }
